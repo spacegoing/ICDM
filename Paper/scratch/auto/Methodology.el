0789abf5c7169c05638faf5fd8dfbe30a2d70fb8
spacegoing <spacegoing@gmail.com>
2017-05-04 21:23:17 +0800

    Start paper

---
+++ Paper/scratch/auto/Methodology.el
@@ -0,0 +1,21 @@
+(TeX-add-style-hook
+ "Methodology"
+ (lambda ()
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (LaTeX-add-labels
+    "cha:methodology"
+    "sec:inference"
+    "sec:exact_inference"
+    "eqn:binary_concave_z"
+    "fig:stmincut"
+    "eq:z_consecutive_constraint"
+    "eqn:posiform"
+    "sec:learning"))
+ :latex)
+
---
+++ Paper/scratch/auto/RelatedWorks.el
@@ -0,0 +1,16 @@
+(TeX-add-style-hook
+ "RelatedWorks"
+ (lambda ()
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "path")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "url")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "nolinkurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperbaseurl")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperimage")
+   (add-to-list 'LaTeX-verbatim-macros-with-braces-local "hyperref")
+   (add-to-list 'LaTeX-verbatim-macros-with-delims-local "path")
+   (LaTeX-add-labels
+    "cha:RelatedWorks"
+    "sec:MRF"
+    "eq:energyfunction_UPH"))
+ :latex)
+
