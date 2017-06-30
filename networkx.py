b15bcaf266d32751cec692d1cf7d03e1c8510fd3
spacegoing <spacegoing@gmail.com>
2017-06-30 19:57:08 +0800

    [New] Graphic networkx

---
+++ networkx.py
@@ -0,0 +1,38 @@
+# -*- coding: utf-8 -*-
+__author__ = 'spacegoing'
+import networkx as nx
+import matplotlib.pyplot as plt
+from networkx import random_geometric_graph
+import networkx as nx
+
+if __name__ == "__main__":
+
+    G = random_geometric_graph(300, 0.25)
+    # position is stored as node attribute data for random_geometric_graph
+    pos = nx.get_node_attributes(G, 'pos')
+
+    # find node near center (0.5,0.5)
+    dmin = 1
+    ncenter = 0
+    for n in pos:
+        x, y = pos[n]
+        d = (x - 0.5) ** 2 + (y - 0.5) ** 2
+        if d < dmin:
+            ncenter = n
+            dmin = d
+
+    # color by path length from node near center
+    p = nx.single_source_shortest_path_length(G, ncenter)
+
+    plt.figure(figsize=(8, 8))
+    nx.draw_networkx_edges(G, pos, nodelist=[ncenter], alpha=0.4)
+    nx.draw_networkx_nodes(G, pos, nodelist=p.keys(),
+                           node_size=80,
+                           node_color=list(p.values()),
+                           cmap=plt.cm.Reds_r)
+
+    plt.xlim(-0.05, 1.05)
+    plt.ylim(-0.05, 1.05)
+    plt.axis('off')
+    plt.savefig('random_geometric_graph.png')
+    plt.show()
